940bec07d72e4f2bb26ddcd665e43a2c0feb00ff
Willian Fakri <wfakri@ciandt.com>
2016-05-18 16:12:54 -0300

    SRP - estrutura inicial, sem refatoração

--- SRP/Produto.cs
+++ SRP/Produto.cs
@@ -8,6 +8,34 @@ namespace SRP
 {
     class Produto
     {
+        public int Codigo { get; set; }
 
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public void Incluir(Produto produto)
+        {
+            //implementacao
+        }
+
+        public void Excluir(int codigoProduto)
+        {
+            //implementacao            
+        }
+
+        public Produto Atualizar(Produto produto)
+        {
+            //implementacao
+            return null;
+        }
+
+        public List<Produto> Listar()
+        {
+            //implementacao
+            return null;
+        }
     }
 }
