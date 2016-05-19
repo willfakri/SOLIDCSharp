0e30c9d3e972682b1442f3f954b2e2b80f43aee2
Willian Fakri <wfakri@ciandt.com>
2016-05-19 11:39:26 -0300

    OCP - Estrutura inicial sem refatoração.

---
+++ OCP/Framework.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    class Framework
+    {
+        public class Retangulo
+        {
+            public double Altura { get; set; }
+            public double Largura { get; set; }
+
+        }
+
+        public class Circulo
+        {
+            public double Raio { get; set; }
+        }
+
+        public class Area
+        {
+            public double CalculaArea(Object[] formas)
+            {
+                double area = 0;
+                foreach(var forma in formas)
+                {
+                    if(forma is Retangulo)
+                    {
+                        Retangulo retangulo = forma as Retangulo;
+                        area += retangulo.Altura * retangulo.Largura;
+                    }
+                    else if(forma is Circulo)
+                    {
+                        Circulo circulo = forma as Circulo;
+                        area += circulo.Raio * circulo.Raio * Math.PI;
+                    }                    
+                }
+
+                return area;
+            }
+        }
+    }
+}
---
+++ OCP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Framework.Retangulo ret1 = new Framework.Retangulo { Altura = 10, Largura = 10 };
+            Framework.Retangulo ret2 = new Framework.Retangulo { Altura = 20, Largura = 20 };
+            Framework.Circulo cir1 = new Framework.Circulo { Raio = 10 };
+            Object[] formas = new Object[] { ret1, ret2, cir1 };
+            Framework.Area area = new Framework.Area();
+            Console.WriteLine(area.CalculaArea(formas));
+            Console.ReadKey();
+        }
+    }
+}
